a0d6d47f2a0164fe8b31bd8cb74b319fb3b6126f
Alok2101 <96423609+Alok2101@users.noreply.github.com>
2023-01-08 17:49:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import SeatReservationForm from "./components/SeatReservationForm";
+export default function App() {
+  return (
+    <div className="App">
+      <SeatReservationForm />
+    </div>
+  );
+}
